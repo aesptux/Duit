a8cf38a8ba71e24be4c752aa126193ec70979589
Adrian Espinosa <aespinosamoreno@gmail.com>
2012-03-27 11:19:36 +0200

    FAQ added

---
+++ faq.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Created on Mar 27, 2012.
+ * Author: Adrián Espinosa
+ * This file is part of Duit.
+ *
+ * Duit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Duit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Duit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+include 'mylib/footer.php';
+include 'mylib/header.php';
+getHeader("Duit | FAQ"); 
+ 
+ 
+?>
+<div class="container">
+	<div class="row">
+		<div class="eight columns centered">
+			<h2>Preguntas frecuentes</h2>
+			<p class="lead">¿Duit tiene algún límite?</p>
+			<p>No, de momento Duit no tiene establecido ningún límite.</p>
+		</div>
+	</div>
+</div>
+<?php 
+getFooter(); 
+
+?>
